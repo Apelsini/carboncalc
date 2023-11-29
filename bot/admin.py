b8e18afb41c40a4c639b8528d8d44e0bb197a662
Apelsini <apelsin652@gmail.com>
2023-11-29 17:07:00 +0300

    1

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from bot.models import PackageMeasures, AdvancedText
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('username', 'email', 'is_superuser', 'is_staff')
+    fieldsets = (
+    (None, {'fields': ('username', 'password')}),
+    ('Personal info', {'fields': ('first_name', 'last_name', 'email',)}),
+    ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    ('Important dates', {'fields': ('last_login', 'date_joined')}),)
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+    (None, {
+        'classes': ('wide',),
+        'fields': ('username', 'email', 'password1', 'password2')}),)
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+    filter_horizontal = ('groups', 'user_permissions',)
+
+# Now register the new UserAdmin...
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+admin.site.register(PackageMeasures)
+
+class AdvancedContentAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
+    summernote_fields = ('text',)
+admin.site.register(AdvancedText, AdvancedContentAdmin)
--- bot/models.py
+++ bot/models.py
@@ -1,86 +1,226 @@
 from django.db import models
+import csv
+import json
+from django.core.files.storage import FileSystemStorage
 
-# Create your models here.
-class Clickboard(models.Model):
-    #author =
+class AdvancedText (models.Model):
+    enabled = models.BooleanField(default=False)
+    menu = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    short = models.CharField(max_length=100)
+    long = models.TextField(blank=True, null=True)
+    text = models.TextField(blank=True, null=True)
+    value = models.TextField(blank=True, null=True)
+    url=models.TextField(blank=True, null=True)
+    parameters=models.TextField(blank=True, null=True)
+
+class Constants(models.Model):
     create_date = models.DateTimeField('date created', auto_now=True)
-    name = models.CharField(max_length=25)
-    commands = models.TextField()
+    author = models.CharField(max_length=75)
+    measure_controlling = models.CharField(max_length=5)  # Управляющий параметр:  номер константы
+    measure_name = models.TextField()  # название меры
+    measure_gravity = models.TextField()  # удельный параметр
+    influence = models.TextField()  # влияние
+    product = models.TextField()  # продукт
+    unit = models.TextField()  # единица измерения
+    comment = models.TextField()  # пояснение
+    perunit_parameter_preinstalled = models.FloatField()  # Удельный параметр - предустановленный
+    perunit_parameter_user = models.FloatField()  # Удельный параметр - предустановленный
+    json_numbers = models.TextField()  #
 
-    def __init__(self):
-        self.commands = []
+class PatternForMeasureSet(models.Model):
+    create_date = models.DateTimeField('date created', auto_now=True)
+    author = models.CharField(max_length=75)
+    measure_controlling = models.CharField(max_length=5)  # Управляющий параметр:  control,
+    measure_name = models.TextField()  # название меры
+    measure_gravity = models.TextField()  # удельный параметр
+    influence = models.TextField()  # влияние
+    product = models.TextField()  # продукт
+    unit = models.TextField()  # единица измерения
+    comment = models.TextField()  # пояснение
+    perunit_parameter_preinstalled = models.FloatField()  # Удельный параметр - предустановленный
 
-    def __str__(self):
-        return self.name
+class Research(models.Model):
+    create_date = models.DateTimeField('date created', auto_now=True)
+    research_name = models.CharField(max_length=175)
+    investments_values = models.TextField()  # сумма инвестиций по набору мер
+    carboneffect_values = models.TextField()  # сумма Углеродэффект по набору мер
+    naturaleffect_values = models.TextField()  # список и суммы по эффекту в натуральном выражении
+    additbenefit_values = models.TextField()  # список и суммы по Допвыгодам
+    constants_selected = models.TextField()  # список и суммы по Допвыгодам
 
-    def add_command(self, command):
-        self.commands += command + '\n'
-        self.save()
+class Scenario(models.Model):
+    create_date = models.DateTimeField('date created', auto_now=True)
+    scenario_name = models.CharField(max_length=175)
+    research = models.ForeignKey(Research, on_delete=models.CASCADE)
+    investments_values = models.TextField()  # сумма инвестиций по набору мер
+    carboneffect_values = models.TextField()  # сумма Углеродэффект по набору мер
+    naturaleffect_values = models.TextField()  # список и суммы по эффекту в натуральном выражении
+    additbenefit_values = models.TextField()  # список и суммы по Допвыгодам
+    constants_selected = models.TextField()  # список и суммы по Допвыгодам
 
-    def edit_command(self, index, new_command):
-        commands_list = self.commands.split('\n')
-        if index < len(commands_list):
-            commands_list[index] = new_command
-            self.commands = '\n'.join(commands_list)
-            self.save()
+class PackageMeasureSet(models.Model):
+    create_date = models.DateTimeField('date created', auto_now=True)
+    packagemeasureset_name = models.CharField(max_length=175)
+    scenario = models.ForeignKey(Scenario, on_delete=models.CASCADE)
+    investments_values = models.TextField()  # сумма инвестиций по набору мер
+    carboneffect_values = models.TextField()  # сумма Углеродэффект по набору мер
+    naturaleffect_values = models.TextField()  # список и суммы по эффекту в натуральном выражении
+    additbenefit_values = models.TextField()  # список и суммы по Допвыгодам
+    constants_selected = models.TextField()  # список и суммы по Допвыгодам
 
-    def delete_command(self, index):
-        commands_list = self.commands.split('\n')
-        if index < len(commands_list):
-            del commands_list[index]
-            self.commands = '\n'.join(commands_list)
-            self.save()
+class MeasureSet(models.Model):
+    create_date = models.DateTimeField('date created', auto_now=True)
+    measureset_name = models.CharField(max_length=175)
+    package = models.ForeignKey(PackageMeasureSet, on_delete=models.CASCADE)
+    investments_values = models.TextField() # сумма инвестиций по набору мер
+    carboneffect_values = models.TextField() # сумма Углеродэффект по набору мер
+    naturaleffect_values = models.TextField() # список и суммы по эффекту в натуральном выражении
+    additbenefit_values = models.TextField() # список и суммы по Допвыгодам
 
-    def search_command(self, index):
-        commands_list = self.commands.split('\n')
-        if index < len(commands_list):
-            return commands_list[index]
+class Measure(models.Model):
+    create_date = models.DateTimeField('date created', auto_now=True)
+    measure_set = models.ForeignKey(MeasureSet, on_delete=models.CASCADE)
+    author = models.CharField(max_length=75)
 
-    def command_count(self):
-        commands_list = self.commands.split('\n')
-        return len(commands_list)
+    measure_textfield = models.TextField()
+    measure_perunit_parameter_preinstalled = models.FloatField()  # Удельный параметр - предустановленный
+    measure_perunit_parameter_user = models.FloatField()  # Удельный параметр - предустановленный
+    measure_json_numbers = models.TextField()  #
 
-# komandy
+    invest_textfield = models.TextField() # measure_name, measure_gravity, influence, product, unit, comment
+    invest_perunit_parameter_preinstalled = models.FloatField() #Удельный параметр - предустановленный
+    invest_perunit_parameter_user = models.FloatField()  # Удельный параметр - предустановленный
+    invest_json_numbers = models.TextField() # значения с 2021 по 2025 г. для numbers = [25, 36, 48, 54] используется json_numbers = json.dumps(numbers)
 
-    def add_title(self, title):
-        self.html += f"<title>{' '.join(title)}</title>\n"
+    carboneffect_textfield = models.TextField()  # measure_name, measure_gravity, influence, product, unit, comment
+    carboneffect_perunit_parameter_preinstalled = models.FloatField()  # Удельный параметр - предустановленный
+    carboneffect_perunit_parameter_user = models.FloatField()  # Удельный параметр - предустановленный
+    carboneffect_json_numbers = models.TextField()  #
 
-    def add_header(self, header):
-        level = header[0]
-        text = ' '.join(header[1:])
-        self.html += f"<h{level}>{text}</h{level}>\n"
+    nateffect1_enabled = models.BooleanField(default=False)
+    nateffect1_textfield = models.TextField()  # measure_name, measure_gravity, influence, product, unit, comment
+    nateffect1_perunit_parameter_preinstalled = models.FloatField()  # Удельный параметр - предустановленный
+    nateffect1_perunit_parameter_user = models.FloatField()  # Удельный параметр - предустановленный
+    nateffect1_json_numbers = models.TextField()  #
 
-    def add_paragraph(self, paragraph):
-        text = ' '.join(paragraph)
-        self.html += f"<p>{text}</p>\n"
+    nateffect2_enabled = models.BooleanField(default=False)
+    nateffect2_textfield = models.TextField()  # measure_name, measure_gravity, influence, product, unit, comment
+    nateffect2_perunit_parameter_preinstalled = models.FloatField()  # Удельный параметр - предустановленный
+    nateffect2_perunit_parameter_user = models.FloatField()  # Удельный параметр - предустановленный
+    nateffect2_json_numbers = models.TextField()  #
 
-    def add_image(self, image):
-        src = image[0]
-        alt = ' '.join(image[1:])
-        self.html += f"<img src='{src}' alt='{alt}'>\n"
+    nateffect3_enabled = models.BooleanField(default=False)
+    nateffect3_textfield = models.TextField()  # measure_name, measure_gravity, influence, product, unit, comment
+    nateffect3_perunit_parameter_preinstalled = models.FloatField()  # Удельный параметр - предустановленный
+    nateffect3_perunit_parameter_user = models.FloatField()  # Удельный параметр - предустановленный
+    nateffect3_json_numbers = models.TextField()  #
 
-    def add_link(self, link):
-        href = link[0]
-        text = ' '.join(link[1:])
-        self.html += f"<a href='{href}'>{text}</a>\n"
+    nateffect4_enabled = models.BooleanField(default=False)
+    nateffect4_textfield = models.TextField()  # measure_name, measure_gravity, influence, product, unit, comment
+    nateffect4_perunit_parameter_preinstalled = models.FloatField()  # Удельный параметр - предустановленный
+    nateffect4_perunit_parameter_user = models.FloatField()  # Удельный параметр - предустановленный
+    nateffect4_json_numbers = models.TextField()  #
 
-    # builder
+    nateffect5_enabled = models.BooleanField(default=False)
+    nateffect5_textfield = models.TextField()  # measure_name, measure_gravity, influence, product, unit, comment
+    nateffect5_perunit_parameter_preinstalled = models.FloatField()  # Удельный параметр - предустановленный
+    nateffect5_perunit_parameter_user = models.FloatField()  # Удельный параметр - предустановленный
+    nateffect5_json_numbers = models.TextField()  #
 
-    def build_html(self):
-        self.html = ""
-        commands_list = self.commands.split('\n')
-        for commandline in commands_list:
-            command = commandline.strip().split()
-            if command[0] == "title":
-                self.add_title(command[1:])
-            elif command[0] == "header":
-                self.add_header(command[1:])
-            elif command[0] == "paragraph":
-                self.add_paragraph(command[1:])
-            elif command[0] == "image":
-                self.add_image(command[1:])
-            elif command[0] == "link":
-                self.add_link(command[1:])
+    additben1_enabled = models.BooleanField(default=False)
+    additben1_textfield = models.TextField()  # measure_name, measure_gravity, influence, product, unit, comment
+    additben1_perunit_parameter_preinstalled = models.FloatField()  # Удельный параметр - предустановленный
+    additben1_perunit_parameter_user = models.FloatField()  # Удельный параметр - предустановленный
+    additben1_json_numbers = models.TextField()  #
+
+    additben2_enabled = models.BooleanField(default=False)
+    additben2_textfield = models.TextField()  # measure_name, measure_gravity, influence, product, unit, comment
+    additben2_perunit_parameter_preinstalled = models.FloatField()  # Удельный параметр - предустановленный
+    additben2_perunit_parameter_user = models.FloatField()  # Удельный параметр - предустановленный
+    additben2_json_numbers = models.TextField()  #
+
+    additben3_enabled = models.BooleanField(default=False)
+    additben3_textfield = models.TextField()  # measure_name, measure_gravity, influence, product, unit, comment
+    additben3_perunit_parameter_preinstalled = models.FloatField()  # Удельный параметр - предустановленный
+    additben3_perunit_parameter_user = models.FloatField()  # Удельный параметр - предустановленный
+    additben3_json_numbers = models.TextField()  #
+
+    additben4_enabled = models.BooleanField(default=False)
+    additben4_textfield = models.TextField()  # measure_name, measure_gravity, influence, product, unit, comment
+    additben4_perunit_parameter_preinstalled = models.FloatField()  # Удельный параметр - предустановленный
+    additben4_perunit_parameter_user = models.FloatField()  # Удельный параметр - предустановленный
+    additben4_json_numbers = models.TextField()  #
+
+    # jsonStr = '[1, 2.1, 3, 4.3]'
+    # aList = json.loads(jsonStr)
+    # print(aList[1] + aList[2])
+    @parameter
+    def json_to_list(self, json_numbers):
+        aList=json.loads(json_numbers)
+        return aList
+
+    @parameter
+    def list_to_json(self, aList):
+        json_numbers = json.dumps(aList)
+        return json_numbers
+
+    @parameter
+    def recalculate(self, scale, multiplier, aList):
+        rlist = []
+        for num in aList:
+            if type(num) == int or type(num) == float:
+                rlist.append(num*multiplier*scale)
             else:
-                print(f"Model Clickboard function Build_html. Unknown command: {command} for instance {self.name}")
-        return self.html
+                rlist.append(0)
+        return rlist
+
+    @parameter
+    def text_to_field(self, measure_controlling, measure_name, measure_gravity, influence, product, unit, comment):
+        out = []
+        out.append(measure_controlling)
+        out.append(measure_name)
+        out.append(measure_gravity)
+        out.append(influence)
+        out.append(product)
+        out.append(unit)
+        out.append(comment)
+        jsonout = json.dumps(out)
+        return jsonout
+  #    measure_controlling = models.CharField(max_length=5) #Управляющий параметр:  control,
+  # measure_name = models.TextField() # название меры
+  #  measure_gravity = models.TextField() # удельный параметр
+   # influence = models.TextField() # влияние
+  #  product = models.TextField() # продукт
+  #  unit = models.TextField() # единица измерения
+   # comment = models.TextField() # пояснение
+
+    @parameter
+    def field_to_text(self, field):
+        aList=json.loads(field)
+        return aList
+
+
+
+# measures are indicated in strings No
+    # 1
+    # 9
+    # 14
+    # 19
+    # 23
+    # 28
+    # 33
+    # 38
+    # 43
+    # 48
+    # 52
+    # 56
+    # 60
+    # 66
+    # 71
+    # 76
+    # 81
+    # 86
+    # 91
+    # 96
+
