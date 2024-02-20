ad65147f9b7558bd85ee04ccb66ecc6a5a3fbc67
Apelsini <apelsin652@gmail.com>
2024-02-20 11:22:04 +0300

    digitalpublicgoods

--- u9botpr/settings.py
+++ u9botpr/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'bot.apps.BotConfig',
 ]
 
+INSTALLED_APPS += ('django_summernote', )
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
